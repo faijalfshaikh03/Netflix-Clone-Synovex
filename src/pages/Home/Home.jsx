097d6c71f4b9be4100667914fa6387e29a064db0
faijalfshaikh <faijalfshaikh@gmail.com>
2026-06-07 09:33:22 +0530

    Initial Commit and boiler plate creation

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Home.css"
+import Navbar from '../../components/Navbar/Navbar'"../../components"
+
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
